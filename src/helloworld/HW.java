7bda29b143ebc0bf4b09179b90cfd6685d3dc599
nrf4 <nrf4@FSTU603026.pti.edu>
2019-05-02 18:12:23 -0400

    First commit

---
+++ src/helloworld/HW.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author nrf4
+ */
+public class HW {
+    
+public static void main(String[] args) {
+  // TODO code application logic here
+  Ferguson();
+}
+
+public static void Ferguson(){
+  System.out.println("Nick Ferguson!");
+}
+
+}
